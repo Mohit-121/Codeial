d42469b3937c95944e2d52d8489b0e4a8a8fa6d0
Mohit <mc27768@gmail.com>
2020-04-02 11:00:36 +0530

    added sign-up

--- controllers/users_controller.js
+++ controllers/users_controller.js
@@ -1,3 +1,5 @@
+const User=require('../models/user');
+
 module.exports.profile = function(req,res){
     return res.render('users_profile',{
         title:'Users Profile'
@@ -10,4 +12,43 @@ module.exports.post= function(req,res){
         title:'Users Post'
     });
     // res.end('<h1>User liked</h1>');
+}
+
+
+// Render the sign up page
+module.exports.signUp=function(req,res){
+    return res.render('user_sign_up',{
+        title: "Codeial | Sign Up"
+    });
+}
+
+
+//Render the sign in page
+module.exports.signIn=function(req,res){
+    return res.render('user_sign_in',{
+        title: "Codeial | Sign In"
+    });
+}
+
+// get the sign up data
+module.exports.create = function(req,res){
+    if(req.body.password!=req.body.confirm_password){
+        return res.redirect('back');
+    }
+
+    User.findOne({email: req.body.email},function(err,user){
+        if(err){console.log('Error in finding user in signing up'); return;}
+        if(!user){
+            User.create(req.body,function(err,user){
+                if(err){console.log('Error in creating user while signing up'); return;}
+                return res.redirect('/users/sign-in');
+            });
+        }else return res.redirect('back');
+    })
+
+}
+
+// sign in and create Session for user
+module.exports.createSession = function(req,res){
+    
 }
--- routes/users.js
+++ routes/users.js
@@ -8,4 +8,12 @@ router.get('/profile',usersController.profile);
 // for user posts
 router.get('/post',usersController.post);
 
+//for user sign-up
+router.get('/sign-up',usersController.signUp);
+
+//for user sign-in
+router.get('/sign-in',usersController.signIn);
+
+router.post('/create',usersController.create);
+
 module.exports=router;
