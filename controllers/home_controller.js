a854cc78a41a639e14051d0ef2f268db1edfa1e9
Mohit <mc27768@gmail.com>
2020-04-15 00:36:54 +0530

    Added post with username

--- controllers/home_controller.js
+++ controllers/home_controller.js
@@ -1,14 +1,10 @@
+const Post=require('../models/post')
 module.exports.home = function(req,res){
-    // read the cookie
-    // console.log(req.cookies);
-    // change the cookie
-    // res.cookie('user_id',25);
-    return res.render('home',{
-        title:"Home"
+    // Populate user of each post
+    Post.find({}).populate('user').exec(function(err,posts){
+        return res.render('home',{
+            title:"Home",
+            posts: posts
+        });
     });
-
-    // Used for static rendering
-    // return res.end('<h1>Express is up for Codeial</h1>');
 }
-
-// module.exports.actionName=function(req,res){}
